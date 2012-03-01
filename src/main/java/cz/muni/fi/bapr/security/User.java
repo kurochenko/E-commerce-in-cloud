f2743824ce19fef6fbce0ea11bb85ce16958a683
Andrej Kuročenko <andrej@kurochenko.net>
2012-03-01 18:28:13 +0100

    created User class for authentication session purpose

---
+++ src/main/java/cz/muni/fi/bapr/security/User.java
@@ -0,0 +1,122 @@
+package cz.muni.fi.bapr.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This object is stored in session for every user that comes to web
+ *
+ * @author Andrej Kuročenko <andrej@kurochenko.net>
+ */
+public class User implements Serializable, UserDetails {
+
+    /**
+     * Users unique identifier used in database
+     */
+    private Long id;
+
+    /**
+     * users username (email is used as username)
+     */
+    private String username;
+
+    /**
+     * hash of user password
+     */
+    private String password;
+
+    /**
+     * users authorities
+     */
+    private Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    public void addAuthority(String authorityName) {
+        authorities.add(new SimpleGrantedAuthority(authorityName));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (authorities != null ? !authorities.equals(user.authorities) : user.authorities != null) return false;
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (authorities != null ? authorities.hashCode() : 0);
+        return result;
+    }
+}
