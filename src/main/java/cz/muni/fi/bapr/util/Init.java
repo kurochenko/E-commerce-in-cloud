abef44582a729c9d01a0f2f6656bc981e6ce95a0
Andrej Kuročenko <andrej@kurochenko.net>
2012-03-01 18:56:04 +0100

    created initialization class

---
+++ src/main/java/cz/muni/fi/bapr/util/Init.java
@@ -0,0 +1,35 @@
+package cz.muni.fi.bapr.util;
+
+import cz.muni.fi.bapr.entity.UserPrivilege;
+import cz.muni.fi.bapr.security.UserRoles;
+import cz.muni.fi.bapr.service.UserPrivilegeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Initializes application on startup. Inserts necessary values into DB
+ *
+ * @author Andrej Kuročenko <andrej@kurochenko.net>
+ */
+@Component
+public class Init {
+
+    @Autowired
+    private UserPrivilegeService userPrivilegeService;
+
+
+    /**
+     * Creates special user privileges
+     */
+    @PostConstruct
+    public void initPrivileges() {
+        if (userPrivilegeService.count() == 0) {
+            UserPrivilege privilege = new UserPrivilege();
+            privilege.setName(UserRoles.ROLE_ADMIN);
+
+            userPrivilegeService.create(privilege);
+        }
+    }
+}
