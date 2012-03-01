43cc94ae6856fc5b09145b756dee28e3a208755e
Andrej Kuročenko <kurochenko@mail.muni.cz>
2012-03-01 16:04:47 +0100

    implemented UserPrivilegeDAO using hibernate

---
+++ src/main/java/cz/muni/fi/bapr/dao/hibernate/UserPrivilegeDAOImpl.java
@@ -0,0 +1,33 @@
+package cz.muni.fi.bapr.dao.hibernate;
+
+import cz.muni.fi.bapr.dao.UserPrivilegeDAO;
+import cz.muni.fi.bapr.entity.UserPrivilege;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * @author Andrej Kuročenko <andrej@kurochenko.net>
+ */
+@Repository
+public class UserPrivilegeDAOImpl extends AbstractParamSearchDAOImpl<UserPrivilege> implements UserPrivilegeDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+
+    public UserPrivilegeDAOImpl() {
+        super(UserPrivilege.class);
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    @Override
+    public UserPrivilege findByName(String name) {
+        return super.findByParam("name", name);
+    }
+}
