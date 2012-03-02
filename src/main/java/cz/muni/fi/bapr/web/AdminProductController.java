3aa7b3ca1b3f793aa8cc2f74d3682fe3c7464d36
Andrej Kuročenko <andrej@kurochenko.net>
2012-03-02 21:52:58 +0100

    added product edit backing methods

--- src/main/java/cz/muni/fi/bapr/web/AdminProductController.java
+++ src/main/java/cz/muni/fi/bapr/web/AdminProductController.java
@@ -7,14 +7,18 @@ import cz.muni.fi.bapr.service.CategoryService;
 import cz.muni.fi.bapr.service.ProductService;
 import cz.muni.fi.bapr.service.VatService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -37,6 +41,10 @@ public class AdminProductController {
     @Autowired
     private VatService vatService;
 
+    @Autowired
+    @Qualifier("springValidator")
+    private Validator validator;
+
 
     @ModelAttribute(MODEL_CATEGORIES)
     public List<Category> prepareCategories() {
@@ -56,13 +64,33 @@ public class AdminProductController {
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String processCreate(@ModelAttribute(MODEL_PRODUCT) @Valid Product product, BindingResult result) {
+    public String processCreate(@ModelAttribute(MODEL_PRODUCT) Product product, BindingResult result) {
 
+        product.setCreated(Calendar.getInstance().getTime());
+
+        validator.validate(product, result);
         if (result.hasErrors()) {
             return "product.form";
         }
 
         productService.create(product);
+        return "redirect:/product/detail/" + product.getId();
+    }
+
+    @RequestMapping("/edit/{id}")
+    public String renderEdit(@PathVariable("id") Long id, Model model) {
+
+        model.addAttribute(MODEL_PRODUCT, productService.find(id));
         return "product.form";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String processEdit(@ModelAttribute(MODEL_PRODUCT) @Valid Product product, BindingResult result) {
+        if (result.hasErrors()) {
+            return "product.form";
+        }
+
+        productService.edit(product);
+        return "redirect:/product/detail/" + product.getId();
     }
 }
